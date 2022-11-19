164278c2b64f719f4a0d2b3636f57a98d574d2d3
Eric Zhang <ericspring08@gmail.com>
2022-11-19 10:24:30 -0600

    see if this works

--- server/main.py
+++ server/main.py
@@ -5,6 +5,10 @@ import processword
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/', methods="GET")
+def index():
+    return jsonify({"message": "Hello World"})
+
 @app.route('/processword', methods=['POST'])
 def ProcessWord():
     words = request.form['words']
